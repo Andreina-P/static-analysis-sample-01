e6ecb613036604750f01287d89f487bed4f376a7
Andreina-P <andreina.pallo@epn.edu.ec>
2026-06-19 14:54:49 -0500

    Corrección import

--- src/test/java/ec/edu/epn/PubPricesTest.java
+++ src/test/java/ec/edu/epn/PubPricesTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @DisplayName("Pub spec")
 public class PubPricesTest {
